5bb01cc8a1e8c80d492d05112b0dca103dd99ec9
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2022-04-03 21:55:59 +0530

    (0.1.0)[Major Alpha Version][added dark/light mode, help, many refactor, CSS design pattern changed, added new fonts]

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+// For smooth-scroll initialization
+new SmoothScroll('a[href*="#"]')
+
 // jQueries for MATERIALIZE CSS
 $(document).ready(function () {
 	// $('.collapsible').collapsible();
@@ -7,6 +10,25 @@ $(document).ready(function () {
 		$(".carousel").carousel("next")
 	}, 2500)
 	$(".dropdown-trigger").dropdown()
-	// $('.modal').modal();
+	$(".modal").modal()
 })
-new SmoothScroll('a[href*="#"]')
+
+const switchDarkForMap = () => {
+	document.body.classList.toggle("dark")
+	// document.querySelector(".divider").classList.toggle("divider__dark")
+	document.querySelector(".modal").classList.toggle("modal__dark")
+	document.querySelector(".modal-footer").classList.toggle("modal-footer__dark")
+}
+
+const switchDarkForIndex = () => {
+	document.body.classList.toggle("dark")
+	// document.querySelector(".divider").classList.toggle("divider__dark")
+	document.getElementById("image-footer").classList.toggle("image-footer__dark")
+	document
+		.querySelector(".h3__algoTable")
+		.classList.toggle("h3__algoTable__dark")
+	document.querySelector(".table-algo").classList.toggle("table-algo__dark")
+	document
+		.querySelector(".h3__atAGlance")
+		.classList.toggle("h3__atAGlance__dark")
+}
