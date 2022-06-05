b4a396852dec3a102ad45d5b0c6f7a1beefbf7a1
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2022-06-06 00:40:56 +0530

    testing pwa, on web (rest optimized)

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,40 +1,65 @@
-const maifyMapApp = "mapify-map-app"
+const mapifyMapApp = "mapify-map-app"
 const assets = [
 	"/",
 	"/index.html",
+	"/disclaimer.html",
 	"/css/map.css",
 	"/css/material-icons.css",
 	"/css/disclaimer.css",
-	"scripts/map.js",
-	"scripts/algorithm.js",
+	"/scripts/map.js",
+	"/scripts/algorithm.js",
 	"/fonts/GoogleSans-Medium.ttf",
 	"/ico/tab-ico.png",
-	"/libs/css/materialize.min.css",
-	"/libs/js/materialize.min.js",
+	"/libs/materialize/css/materialize.min.css",
+	"/libs/materialize/js/materialize.min.js",
+	"/utils/lottie-player.js",
+	"/utils/material-theme-control.js",
+	"/utils/animations/86746-loading-blash.json",
 	"/maps/Abstract_Maps/absMap3.png",
-	"./maps/Real_Maps/gMaps5.png",
-	"./maps/Real_Maps/gMaps6.png",
-	"./maps/Real_Maps/gMaps7.png",
-	"./maps/Real_Maps/gMaps8.png",
-	"./maps/Real_Maps/gMaps9.png",
-	"./maps/Industries/indMap1.jpg",
-	"./maps/Industries/indMap2.jpg",
-	"./maps/Industries/indMap3.jpg",
+	"/maps/Real_Maps/gMaps5.png",
+	"/maps/Real_Maps/gMaps6.png",
+	"/maps/Real_Maps/gMaps7.png",
+	"/maps/Real_Maps/gMaps8.png",
+	"/maps/Real_Maps/gMaps9.png",
+	"/maps/Industries/indMap1.jpg",
+	"/maps/Industries/indMap2.jpg",
+	"/maps/Industries/indMap3.png",
 	"/sounds/party-trumpet.wav",
 ]
 
-self.addEventListener("install", (installEvent) => {
-	installEvent.waitUntil(
-		caches.open(maifyMapApp).then((cache) => {
-			cache.addAll(assets)
-		})
-	)
+self.addEventListener("install", (e) => {
+	e.waitUntil(async () => {
+		const cache = await caches.open(mapifyMapApp)
+		await cache.addAll(assets)
+	})
 })
 
-self.addEventListener("fetch", (fetchEvent) => {
-	fetchEvent.respondWith(
-		caches.match(fetchEvent.request).then((res) => {
-			return res || fetch(fetchEvent.request)
+// fetch new cache data
+self.addEventListener("fetch", (e) => {
+	e.respondWith(async () => {
+		const r = await caches.match(e.request)
+		if (r) {
+			return r
+		}
+		const response = await fetch(e.request)
+		const cache = await caches.open(cacheName)
+		cache.put(e.request, response.clone())
+		return response
+	})
+})
+
+// clear old caches
+self.addEventListener("activate", (e) => {
+	e.waitUntil(
+		caches.keys().then((keyList) => {
+			return Promise.all(
+				keyList.map((key) => {
+					if (key === mapifyMapApp) {
+						return
+					}
+					return caches.delete(key)
+				})
+			)
 		})
 	)
 })
